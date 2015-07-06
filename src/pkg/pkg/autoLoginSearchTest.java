dd7e09dace519e1ec8494d2a32f2869e1a3e441f
sophie2805 <sophie2805@163.com>
2015-07-06 12:09:03 +0800

    Update autoLoginSearchTest.java

--- src/pkg/pkg/autoLoginSearchTest.java
+++ src/pkg/pkg/autoLoginSearchTest.java
@@ -1,8 +1,5 @@
 package pkg;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -17,27 +14,29 @@ import java.util.concurrent.TimeUnit;
 /**
  * Created by Sophie on 15/7/4.
  */
-public class autoLoginSearchTest {
-    private WebDriver dr;
-    private String url;
+public class AutoLoginSearchTest {
+    public WebDriver dr;
+    public String url;
 
-    @Before
-    public void setup() throws Exception {
-        dr = new FirefoxDriver();
-        url = new String("https://www.wacai.com/user/user.action?url=http%3A%2F%2Fbbs.wacai.com%2Fportal.php");
-        dr.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    public void setUp(WebDriver dr, String url){
+        this.dr = dr;
+        this.url = url;
+        this.dr.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
     }
 
-    @Test
-    public void actionTest() throws Exception {
+    public WebDriver login(AutoLoginSearchTest a) throws Exception {
         //登录
-        dr.get(url);
-        dr.findElement(By.id("account")).clear();
-        dr.findElement(By.id("account")).sendKeys("******");
-        dr.findElement(By.id("pwd")).clear();
-        dr.findElement(By.id("pwd")).sendKeys("******");
-        dr.findElement(By.id("login-btn")).click();
-        //打开理财规划子版块，然后关闭左侧导航栏
+        a.dr.get(a.url);
+        a.dr.findElement(By.id("account")).clear();
+        a.dr.findElement(By.id("account")).sendKeys("******");
+        a.dr.findElement(By.id("pwd")).clear();
+        a.dr.findElement(By.id("pwd")).sendKeys("******");
+        a.dr.findElement(By.id("login-btn")).click();
+        return a.dr;
+    }
+
+    public WebDriver switchWindow(WebDriver dr)throws Exception{
+        //打开理财规划子版块
         dr.findElement(By.linkText("理财规划")).click();
         //用WindowHandle的方式获取新开页面
         String currentWindow = dr.getWindowHandle();
@@ -48,7 +47,12 @@ public class autoLoginSearchTest {
         }
         System.out.println(dr.getCurrentUrl());
         dr.manage().window().maximize();
+        //然后关闭左侧导航栏
         dr.findElement(By.cssSelector("a.comiis_left_closes")).click();
+        return dr;
+    }
+
+    public WebDriver mouseMove(WebDriver dr)throws Exception{
 
         //点开搜索类型下拉框，将鼠标移动到用户上并选择
         Actions act = new Actions(dr);
@@ -57,8 +61,10 @@ public class autoLoginSearchTest {
         act.click(dropDown).perform();
         act.moveToElement(user).click().perform();
         act.moveByOffset(20, 30).click().perform();
-        //为了把过程看的更仔细，所以在此休眠1s
-        Thread.sleep(1000);
+        return dr;
+    }
+
+    public WebDriver search(WebDriver dr)throws Exception{
         //找到搜索输入框，并输入关键字，然后点击搜索按钮
         WebElement input = dr.findElement(By.id("comiis_twtsc_txt"));
         input.clear();
@@ -66,10 +72,10 @@ public class autoLoginSearchTest {
         dr.findElement(By.id("comiis_twtsc_btn")).click();//Click之后FireFox新开了一个页面
 
         //用WindowHandle+页面title来切换dr至我们想要的窗口
-        currentWindow = dr.getWindowHandle();
-        handles = dr.getWindowHandles();
+        String currentWindow = dr.getWindowHandle();
+        Set<String> handles = dr.getWindowHandles();
         //System.out.println(handles.size());
-        h = handles.iterator();
+        Iterator h = handles.iterator();
         while (h.hasNext()) {
             dr = dr.switchTo().window((String) h.next());
             //System.out.println(dr.getTitle());
@@ -82,11 +88,23 @@ public class autoLoginSearchTest {
         for (int i = 0; i < searchResult.size(); i++) {
             System.out.println(searchResult.get(i).getAttribute("title"));
         }
-
+        return dr;
     }
 
-    @After
-    public void tearDown() throws Exception {
+    public void tearDown(WebDriver dr) throws Exception {
         dr.quit();
+    }
+
+    public static void main(String[] args) throws Exception {
+        WebDriver ff = new FirefoxDriver();
+        String url ="https://www.wacai.com/user/user.action?url=http%3A%2F%2Fbbs.wacai.com%2Fportal.php";
+        AutoLoginSearchTest autoLoginSearch = new AutoLoginSearchTest();
+        autoLoginSearch.setUp(ff,url);
+        WebDriver afterLogin = autoLoginSearch.login(autoLoginSearch);
+        WebDriver childForum = autoLoginSearch.switchWindow(afterLogin);
+        WebDriver mouseAction = autoLoginSearch.mouseMove(childForum);
+        WebDriver searchResult = autoLoginSearch.search(mouseAction);
+        autoLoginSearch.tearDown(searchResult);
+
     }
 }
